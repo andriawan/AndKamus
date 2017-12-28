dade4013ec3fe40206deaeb6b6e3d16269878db3
Muhammad Irwan Andriawan <andriawan2014@gmail.com>
2017-12-28 23:45:26 +0700

    improvement main

--- src/app.cpp
+++ src/app.cpp
@@ -11,11 +11,9 @@
 // entry point of the apps
 int main(int argc, char const *argv[])
 {
-	AndKamus ak;
-	ak.evaluateParameter(argc, argv[1]);
 	// argv[0] = the name of the app
 	// argv[1] = the first argument
-	//cout << "the argument is " << argv[1] << endl;
-	/* code */
+	AndKamus ak;
+	ak.evaluateParameter(argc, argv[1]);
 	return 0;
 }
