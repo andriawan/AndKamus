be785c261169fc4a595e0f45267aaaa47bfb2cf2
Muhammad Irwan Andriawan <andriawan2014@gmail.com>
2017-12-28 12:06:38 +0700

    inisialisasi header yang berisi AndKamus class

---
+++ AndKamus.h
@@ -0,0 +1,19 @@
+#ifndef ANDKAMUS_H
+#define ANDKAMUS_H
+#include <iostream>
+
+using namespace std;
+
+class AndKamus {
+
+public:
+	AndKamus();
+	int showHelp(int counter, const char *args);
+	string getVersion( return Version);
+private:
+	string Version;
+
+
+};
+
+#endif
