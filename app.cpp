7f6ac38b5dd0b6e24939675cce9029dbd0cc498f
Muhammad Irwan Andriawan <andriawan2014@gmail.com>
2017-12-28 09:58:23 +0700

    first init

---
+++ app.cpp
@@ -0,0 +1,21 @@
+/*
+ * Create By : Muhammad Irwan Andriawan
+ * This is CLI Apps as Dictionary
+ * Name: AndKamus (ID - ENG, ENG - ID) Dictionary
+ * Version: 0.1
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+// entry point of the apps
+int main(int argc, char const *argv[])
+{
+	// argv[0] = the name of the app
+	// argv[1] = the first argument
+	cout << "the argument is " << argv[1] << endl;
+	/* code */
+	return 0;
+}
